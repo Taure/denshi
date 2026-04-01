c84bb6c13b977b97967ef5cd8d364f9084274fc4
Daniel Widgren <daniel.widgren@gmail.com>
2026-04-01 22:13:00 +0200

    fix: support app env config and library mode
    - Auto-start gateway when token is set via application env
    - Coerce string tokens (from sys.config env vars) to binary
    - No-op when no config is set (library mode)

--- src/denshi_app.erl
+++ src/denshi_app.erl
@@ -4,8 +4,23 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    Config = application:get_all_env(denshi),
-    denshi_sup:start_link(maps:from_list(Config)).
+    case application:get_env(denshi, token) of
+        {ok, _} ->
+            Config = normalize(maps:from_list(application:get_all_env(denshi))),
+            denshi_sup:start_link(Config);
+        undefined ->
+            {ok,
+                spawn_link(fun() ->
+                    receive
+                        stop -> ok
+                    end
+                end)}
+    end.
 
 stop(_State) ->
     ok.
+
+normalize(#{token := Token} = Config) when is_list(Token) ->
+    Config#{token := unicode:characters_to_binary(Token)};
+normalize(Config) ->
+    Config.
